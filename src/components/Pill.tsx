e7f76bcfdf9ca3767e3dae7f20287069f9cc3b1e
Vadym Shcherbakov <vadym.uxd@gmail.com>
2025-06-22 10:38:50 +0100

    pill component UI restyle

--- src/components/Pill.tsx
+++ src/components/Pill.tsx
@@ -11,11 +11,11 @@ export function Pill({ label, isActive, onClick }: PillProps) {
     <button
       onClick={onClick}
       className={`
-        rounded-[36px] px-3 py-2 transition-all cursor-pointer label-mini
+        rounded-[36px] px-4 py-2.5 transition-all cursor-pointer label-mini
         ${
           isActive
             ? 'bg-white text-[var(--color-black)] hover:opacity-80'
-            : 'bg-transparent text-[var(--color-white)] outline outline-1 outline-white hover:bg-white hover:text-[var(--color-black)]'
+            : 'bg-[var(--color-grey-dark)] text-[var(--color-grey-light)] hover:bg-white hover:text-[var(--color-black)]'
         }
       `}
     >
