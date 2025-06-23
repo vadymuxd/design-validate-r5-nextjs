f4d2717dd49d234714427d409cc8d8722498115f
Vadym Shcherbakov <vadym.uxd@gmail.com>
2025-06-23 17:10:52 +0100

    Deploy fix

--- src/app/methods/layout.tsx
+++ src/app/methods/layout.tsx
@@ -1,5 +1,5 @@
 import { Metadata } from 'next';
-import React, { ReactNode } from 'react';
+import React from 'react';
 
 export const metadata: Metadata = {
   title: "Design Validation Methods | Design. Validate",
